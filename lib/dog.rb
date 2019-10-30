d2d609124d3ca2aa87b86ebe0f39919ef0e367c5
Ali Saidik <saida130.210@gmail.com>
2019-10-30 01:36:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 class Dog 
-  def initialize(name)
-    @name = name 
+  def initialize(name, breed)
+    @name = name
+    @breed = breed 
   end 
-  def name=(name)
-    @name = name 
-  end 
-  def name 
-    @name 
-  end 
+  # def name=(name)
+  #  @name = name 
+  #end 
+  #def name 
+  #  @name 
+  #=end 
 end 
